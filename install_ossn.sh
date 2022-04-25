0533edcd08dac25765809aac7a449fdf91aef240
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2022-04-25 17:23:02 -0400

    Update install_ossn.sh

--- install_ossn.sh
+++ install_ossn.sh
@@ -27,7 +27,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install unzip -y
 
 # Install web components
-echo 'Installing Web Components'
+echo 'Installing Web Components...'
 echo
 sudo apt install apache2 mariadb-common mariadb-server php7.4 php7.4-mysql php7.4-zip php7.4-xml php7.4-curl php7.4-mb* php7.4-gd php7.4-dom php7.4-zip php7.4-json -y
 
@@ -38,3 +38,8 @@ wget https://www.opensource-socialnetwork.org/download_ossn/latest/build.zip
 unzip build.zip -d var/www/html/
 sudo chown -r www-data:www-data /var/www/html/ossn
 sudo mkdir var/www/html/ossn/data
+
+# MySQL Configuration
+echo "Press Enter:"
+sudo mysql -u root -p < $HOME/nextcloud-automation/mysql_setup.sql
+sudo systemctl restart apache2
